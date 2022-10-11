8bc54ffd760e8e33eb661203eef8bdf028262e4d
Hussein Rammal <59229767+HusseinOP@users.noreply.github.com>
2022-10-11 13:02:18 -0700

    Update App.java

--- App.java
+++ App.java
@@ -14,3 +14,25 @@ public class App
        parse.run(null);
     }
 }
+
+
+
+/** this is for parsing into xml
+ * public class App 
+{
+    public static void main( String[] args ) throws JAXBException
+    {
+    	Parking user= new Parking();
+        user.setLatitude(100);
+        user.setLongitude(100);
+        user.setNbSpots(50);
+        user.setSpotsavailable(20);
+        JAXBContext contextObj = JAXBContext.newInstance(Parking.class);  
+        Marshaller m= contextObj .createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);  
+        File file =new File("/home/hussein/Desktop/park.xml");
+        m.marshal(user, file);   
+    }
+}
+ *
+ */
